ddb6076c5c260c8b802a8b2bedf5b2d406c29e35
buddhika85 <buddhika2006system@gmail.com>
2018-03-15 17:27:04 +0530

    Ran migrations and EF configurations

--- EF/Migrations/201802260735370_InitialEmployeeDataPopulation.cs
+++ EF/Migrations/201802260735370_InitialEmployeeDataPopulation.cs
@@ -7,7 +7,7 @@ namespace EF.Migrations
     {
         public override void Up()
         {
-            Sql("insert into TBL_EMPLOYEE (first_name, last_name, salary, permanent, department_fk_id) values ('John', 'James', 100000, 1, 13)");
+            Sql("insert into TBL_EMPLOYEE (first_name, last_name, salary, permanent, department_fk_id) values ('John', 'James', 100000, 1, (select ID from TBL_DEPARTMENT where Name = 'Admin'))");
         }
         
         public override void Down()
